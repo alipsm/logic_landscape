4d50c32a2ae82e3b6e2c5772a575c630596b68a8
alipsm <stooormix@gmail.com>
2024-01-14 23:45:52 +0330

    init project & install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    escreens: {
+      sm: '480px',
+      md: '768px',
+      xl: '1440px',
+    },
+    colors: {
+      maroon: '#0f090c8f',
+      champagne: '#ffede099',
+      ivory: '#ffffff52',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {}
+  },
+  plugins: [],
+}
