deba23c81a87d73588ef87a92e62616312058671
alipsm <stooormix@gmail.com>
2024-01-15 23:22:07 +0330

    install and use react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { Route, Routes } from "react-router";
+import Dashboard from "./components/dashboard/Dashboard";
+
 function App() {
   return (
-    <div>
+    <div className="relative w-screen h-screen p-5 bg-champagne">
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />}>
+        </Route>
+      </Routes>
     </div>
   );
 }
