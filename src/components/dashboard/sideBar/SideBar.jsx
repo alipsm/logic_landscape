9e7f4e74be08d8b97de30d0c73c7521baf7e52da
alipsm <stooormix@gmail.com>
2024-01-16 16:25:57 +0330

    add all layout in the SideBar file

---
+++ src/components/dashboard/sideBar/SideBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import AdminCard from './layout/adminCard'
+import MacControllerAnimation from '../../../animations/macController'
+import Hr from '../../../utils/graphics/hr/Hr'
+import Contributers from './layout/contributers'
+import Sections from './layout/sections'
+import Banner from './layout/banner'
+
+export default function SideBar() {
+  return (
+    <div className=' flex flex-col justify-between boxShadow relative h-full w-52 bg-maroon rounded-3xl px-4 py-5'>
+      <div>
+        <MacControllerAnimation />
+        <AdminCard />
+        <Hr />
+        <Sections />
+        <Hr />
+        <Contributers />
+      </div>
+      <Banner />
+    </div>
+  )
+}
