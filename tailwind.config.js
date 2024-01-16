87e01467a7d4b74c402b5664afbf86f5e5306ca7
alipsm <stooormix@gmail.com>
2024-01-16 16:16:55 +0330

    install fontawesome and set config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
       maroon: '#0f090c8f',
       champagne: '#ffede099',
       ivory: '#ffffff52',
+      greenCustome:'#53C22B',
+      yellowCustome:'#E6C02A',
+      redCustome:'#FF5A52',
+      white: '#ffffff',
+      hrLine:"radial-gradient(50% 50% at 50% 50%, #CC8B8B 0%, rgba(163, 59, 59, 0.00) 100%)"
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
