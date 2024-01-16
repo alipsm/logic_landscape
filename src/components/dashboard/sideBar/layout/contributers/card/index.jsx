d6f5586a1bf218aa5688965a4117099206d62034
alipsm <stooormix@gmail.com>
2024-01-16 16:23:25 +0330

    create contributers sideBar layout

---
+++ src/components/dashboard/sideBar/layout/contributers/card/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import user_pic from './img/contributer.jpeg'
+
+export default function Card() {
+  return (
+    <div className='flex justify-start items-center py-3 pl-4 pr-2 w-full gap-3'>
+      <img src={user_pic} alt='ali parsamanesh' className=' w-6 h-6 rounded-full'/>
+      <span className='capitalize overflow-hidden whitespace-nowrap text-ellipsis'>ali parsamanesh</span>
+    </div>
+  )
+}
