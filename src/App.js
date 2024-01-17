8575b179fd1732a57f5033dd0626b21c5f8c4691
alipsm <stooormix@gmail.com>
2024-01-17 22:35:53 +0330

    set new route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Dashboard from "./components/dashboard/Dashboard";
+import Multithreading from "./pages/multithreading";
+import Info from "./pages/info";
 
 function App() {
   return (
     <div className="relative w-screen h-screen p-5 bg-champagne">
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />}>
+        <Route path="/dashboard/*" element={<Dashboard />}>
+          <Route path="info" element={<Info />} />
+          <Route path={Multithreading.name} element={<Multithreading />} />
+          <Route path="*" element={<Navigate to={"/dashboard/info"} replace/>} />
         </Route>
+        <Route path="*" element={<Navigate to={"/dashboard"} replace/>} />
       </Routes>
     </div>
   );
