c03673655d74100e1a74ffda709e2e4aaaf1a5cd
alipsm <stooormix@gmail.com>
2024-01-18 17:11:20 +0330

    add hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, HashRouter as Router } from "react-router-dom";
 
 import App from './App';
 import "./config/fontAwesomeConfig"
@@ -9,8 +9,8 @@ import "./assets/css/index.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
     <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
