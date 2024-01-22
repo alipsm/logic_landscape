a2fc949812a712639f9d5b42fc78e24e9d83f464
alipsm <stooormix@gmail.com>
2024-01-22 23:51:12 +0330

    create helpers IIFE for multithreading page

---
+++ src/pages/multithreading/helpers/index.jsx
@@ -0,0 +1,43 @@
+const multithreadingHelper = (() => {
+
+
+    const getRandomNum = (max = 10) => Math.floor(Math.random() * max)
+    const isBusy = (item) => item?.status === "busy"
+    const isWait = (item) => item?.status === "wait" || item === "wait"
+    const isFree = (item) => item?.status === "free" || item === "free"
+    const roundToUp = (num) => Math.floor(num / 10) * 10;
+    const getNumberAtString = (str) => str?.match(/[\d\.]+/g)?.join("")
+    const getPointsWithDeg = (mainDeg, pointDeg) => mainDeg + 10 >= pointDeg & mainDeg <= pointDeg
+
+    function getCurrentElementRotation(el) {
+        try {
+            var st = window.getComputedStyle(el, null);
+            var tm = st.getPropertyValue("-webkit-transform") ||
+                st.getPropertyValue("-moz-transform") ||
+                st.getPropertyValue("-ms-transform") ||
+                st.getPropertyValue("-o-transform") ||
+                st.getPropertyValue("transform") ||
+                "none";
+            if (tm != "none") {
+                var values = tm.split('(')[1].split(')')[0].split(',');
+                var angle = Math.round(Math.atan2(values[1], values[0]) * (180 / Math.PI));
+                return (angle < 0 ? angle + 360 : angle);
+            }
+            return 0;
+        } catch (error) {
+            return 0;
+        }
+    }
+
+    return {
+        getCurrentElementRotation,
+        getRandomNum,
+        isBusy,
+        isWait,
+        isFree,
+        roundToUp,
+        getNumberAtString,
+        getPointsWithDeg
+    };
+})();
+export default multithreadingHelper;
