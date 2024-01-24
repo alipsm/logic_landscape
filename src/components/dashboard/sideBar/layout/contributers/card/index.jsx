b1e298b5e2368b1efe52db68d710e792a5514db4
alipsm <stooormix@gmail.com>
2024-01-24 23:54:24 +0330

    handle minimull position for contributers&sections cards

--- src/components/dashboard/sideBar/layout/contributers/card/index.jsx
+++ src/components/dashboard/sideBar/layout/contributers/card/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import user_pic from './img/contributer.jpeg'
 
-export default function Card() {
+export default function Card({minimull}) {
   return (
-    <div className='flex justify-start items-center py-3 pl-4 pr-2 w-full gap-3 hover:opacity-80 transition-opacity cursor-pointer'>
-      <img src={user_pic} alt='ali parsamanesh' className=' w-6 h-6 rounded-full'/>
-      <span className='capitalize overflow-hidden whitespace-nowrap text-ellipsis'>ali parsamanesh</span>
+    <div className={`flex  items-center ${minimull?"p-2 justify-center":"py-3 pl-4 pr-2 justify-start"} w-full gap-3 hover:opacity-80 transition-opacity cursor-pointer`}>
+      <img src={user_pic} alt='ali parsamanesh' className={`${minimull?"w-f ull h-f ull":""} w-6 h-6 rounded-full`}/>
+      <span hidden={minimull} className='capitalize overflow-hidden whitespace-nowrap text-ellipsis'>ali parsamanesh</span>
     </div>
   )
 }
--- src/components/dashboard/sideBar/layout/sections/card/index.jsx
+++ src/components/dashboard/sideBar/layout/sections/card/index.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
 
-export default function Card({ text, ico, link }) {
+export default function Card({ text, ico, link , minimull }) {
     return (
         <Link to={link}>
-            <div className='flex justify-end cursor-pointer items-center py-3 pl-4 pr-2 w-full gap-3 text-white hover:opacity-80 transition-opacity'>
-                <span className='capitalize overflow-hidden whitespace-nowrap text-ellipsis text-xs opacity-55' dir='rtl'>{text}</span>
+            <div className={`flex  cursor-pointer items-center ${minimull?" justify-center":"justify-end pl-4 pr-2"} py-3 w-full gap-3 text-white hover:opacity-80 transition-opacity`}>
+                <span hidden={minimull} className='capitalize overflow-hidden whitespace-nowrap text-ellipsis text-xs opacity-55' dir='rtl'>{text}</span>
                 <FontAwesomeIcon icon={`fa-solid ${ico}`} width={15} height={15} />
             </div>
         </Link>
