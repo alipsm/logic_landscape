8d805fe53583e654be371836dc6ba634ef7a41a9
alipsm <stooormix@gmail.com>
2024-01-25 18:17:15 +0330

    set link and handle minimull style for contributers layout

--- src/components/dashboard/sideBar/layout/contributers/card/index.jsx
+++ src/components/dashboard/sideBar/layout/contributers/card/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import user_pic from './img/contributer.jpeg'
-
-export default function Card({minimull}) {
+export default function Card({ img, fullName, minimull }) {
   return (
-    <div className={`flex  items-center ${minimull?"p-2 justify-center":"py-3 pl-4 pr-2 justify-start"} w-full gap-3 hover:opacity-80 transition-opacity cursor-pointer`}>
-      <img src={user_pic} alt='ali parsamanesh' className={`${minimull?"w-f ull h-f ull":""} w-6 h-6 rounded-full`}/>
-      <span hidden={minimull} className='capitalize overflow-hidden whitespace-nowrap text-ellipsis'>ali parsamanesh</span>
-    </div>
+    <a href="https://github.com/alipsm" target='_blank' className={`flex items-center ${minimull ? "p-2 justify-center" : "py-3 pl-4 pr-2 justify-start"} w-full gap-3 hover:opacity-80 transition-opacity cursor-pointer`}>
+        <img src={img} alt={fullName} className={`${minimull ? "w-f ull h-f ull" : ""} w-6 h-6 rounded-full`} />
+        <span hidden={minimull} className='capitalize overflow-hidden whitespace-nowrap text-ellipsis'>{fullName}</span>
+    </a>
   )
 }
--- src/components/dashboard/sideBar/layout/contributers/index.jsx
+++ src/components/dashboard/sideBar/layout/contributers/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Card from './card'
 import ListItems from '../../../../../elements/listItems'
+import Userpic from './img/contributer.jpeg'
 
-export default function Contributers() {
+export default function Contributers({minimull}) {
+    const apiData=[
+        {img:Userpic,name:"ali parsamanesh" , link:"https://github.com/alipsm"}
+    ]
     return (
-        <ListItems title={"مشارکت کننده گان"} liItems={[
-            <Card />
-        ]} />
+        <ListItems title={"مشارکت کننده گان"} minimull={minimull} liItems={apiData.map(item=>(
+            <Card fullName={item.name} img={item.img} minimull={minimull}/>
+        ))} />
     )
 }
